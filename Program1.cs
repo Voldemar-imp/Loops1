55cc49c5b4881aff613aadbf58c4f962f1273130
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-07-04 19:52:03 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conditionalOperatorsAndLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int repetitions;
+
+            Console.Write("Введите сообщение: ");
+            userMessage = Console.ReadLine();
+            Console.Write("Введите количество повторений:");
+            repetitions = Convert.ToInt32(Console.ReadLine());
+
+            while (repetitions-- > 0)
+            {
+                Console.WriteLine(userMessage);
+            }
+        }
+    }
+}
